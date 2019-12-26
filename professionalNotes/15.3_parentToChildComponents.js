0af9ba8dfcee889c23a7e3edfa057f92452c1cd1
katlew <kat.lew.junk@gmail.com>
2019-12-26 09:04:16 -0800

    ch 15

---
+++ professionalNotes/15.3_parentToChildComponents.js
@@ -0,0 +1,26 @@
+// pass props down to child componets
+
+import React from 'react';
+
+class Parent extends React.Component {
+    render() {
+        const variable = 5;
+        return(
+            <div>
+                <Child message="message for child" />
+                <Child message={variable} />
+            </div>
+        );
+    }
+}
+
+class Child extends React.Component {
+   render() {
+       return <h1>{this.props.message}</h1> 
+   }
+}
+
+export default Parent;
+
+// <Parent /> renders 2 <Child /> components
+// passing values down
