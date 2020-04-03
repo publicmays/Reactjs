d1e1b731e14c51c8aa35228fcf131bbba86c0d3c
katlew <kat.lew.junk@gmail.com>
2020-04-03 09:04:55 -0700

    finish 9-pagination/

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import { FEED_QUERY } from './LinkList'
+import { LINKS_PER_PAGE } from '../constants';
 
 const POST_MUTATION = gql `
 mutation PostMutation($description: String!, $url: String!) {
@@ -44,11 +45,15 @@ class CreateLink extends Component {
                 variables={{description, url}}
                 onCompleted={() => this.props.history.push('/')}
                 update={(store, {data: {post}}) => {
-                    const data = store.readQuery({query: FEED_QUERY});
+                    const first = LINKS_PER_PAGE;
+                    const skip = 0;
+                    const orderBy = 'createdAt_DESC';
+                    const data = store.readQuery({query: FEED_QUERY,
+                    variables: {first, skip, orderBy}});
                     data.feed.links.unshift(post);
                     store.writeQuery({
                         query: FEED_QUERY,
-                        data
+                        data, variables: { first, skip, orderBy}
                     });
                 }}>
                    {
